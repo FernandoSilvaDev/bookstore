c9ec55c0c407eb80ffa8ef1352ada9eecd7e4a57
Fernando Silva <artfernando.carlos@gmail.com>
2025-11-26 17:01:24 -0300

    Configurado autenticação e tests

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "order",
     "product",
     "debug_toolbar",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -132,6 +133,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
 
 INTERNAL_IPS = [
--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -1,6 +1,7 @@
 import json
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIClient
 
 
@@ -14,6 +15,10 @@ class TestOrderViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        self.user = UserFactory()
+        token = Token.objects.create(user=self.user)  # added
+        token.save()  # added
+
         self.category = CategoryFactory(title='technology')
         self.product = ProductFactory(title='mouse', price=100, category=[self.category])
         self.order = OrderFactory(product=[self.product])
