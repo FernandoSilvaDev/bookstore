2398bd063dc144dbb61ec317a8ba57135ce28ddf
Fernando Silva <artfernando.carlos@gmail.com>
2025-12-10 15:05:02 -0300

    mudando Setting

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/home/FernandoSilvaDev/bookstore/staticfiles"
 
 
 # Default primary key field type
