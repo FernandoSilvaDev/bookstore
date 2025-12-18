8699249f04018faa1f68790aa59ba2b0ea87c23e
Fernando Silva <artfernando.carlos@gmail.com>
2025-12-18 12:54:13 -0300

    Ajustes no arquivo settings e views

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/home/FernandoSilvaDev/bookstore/staticfiles"
+STATIC_ROOT = "/home/fernandosilvadev/bookstore/staticfiles"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 # Default primary key field type
